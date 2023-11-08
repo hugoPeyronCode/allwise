6aba3676314ef75f139627341423bfb0c4b02006
Hugo Peyron <hugopeyron@gmail.com>
2023-11-08 11:34:01 +0100

    Initial Commit

---
+++ allwise/allwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  allwiseApp.swift
+//  allwise
+//
+//  Created by Hugo Peyron on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct allwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
